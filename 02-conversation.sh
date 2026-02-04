9c8ab05887cf3532493957d336b859b33f42557f
harshad <mohammadbasha8656@gmail.com>
2026-02-04 14:40:34 +0530

    hello world commited

---
+++ 02-conversation.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Suresh::Hello Ramesh, How are you?"
+echo "Ramesh:: Hi Suresh, I am fine thanks for asking. How are you doing?"
+echo "Suresh:: doing fine. what are you learning Ramesh?"
+echo "Ramehs:: I am learnng shell. What about you?"
