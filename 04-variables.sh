50c520646618b67412f4bfa80c48d1bd4396e600
harshad <mohammadbasha8656@gmail.com>
2026-02-07 08:08:40 +0530

    corrected

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON=$1
+PERSON2=$2
+
+echo "$1:: Hello $2, How are you?"
+echo "$2:: Hi $1, I am fine thanks for asking. How are you doing?"
+echo "$1:: doing fine. what are you learning $2?"
+echo "$2:: I am learning shell. What about you?"
+echo "$1:: I have started linux $2"
---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please Enter your name::"
+read USER_NAME
+
+echo "User name is $USER_NAME"
+echo "Please enter your password::"
+read -s PASSWORD
+echo "password is $PASSWORD"
