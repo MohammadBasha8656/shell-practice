ed2e6d0dedf846f13c66a5c7e1d6ef9b3dff73e0
harshad <mohammadbasha8656@gmail.com>
2026-02-07 07:10:07 +0530

    added somthing more

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=Trump
+PERSON2=Puthin
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
+echo "$PERSON1:: doing fine. what are you learning $PERSON2?"
+echo "$PERSON2:: I am learning shell. What about you?"
+echo "$PERSON2:: "I have started linux $PERSON1"
