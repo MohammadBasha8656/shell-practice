4e1a789ce809445bc686e32798d5524f84d33314
harshad <mohammadbasha8656@gmail.com>
2026-02-07 07:24:30 +0530

    corrected

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking. How are you doing?"
 echo "$PERSON1:: doing fine. what are you learning $PERSON2?"
 echo "$PERSON2:: I am learning shell. What about you?"
-echo "$PERSON2:: "I have started linux $PERSON1"
+echo "$PERSON1:: I have started linux $PERSON2
